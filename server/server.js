e079f9906e829f2973dd0c7e9f664d43a11a44bf
Ryan Templin <tempjuice@hotmail.com>
2017-08-29 13:01:03 -0500

    Giphy API random and search functional.

--- server/server.js
+++ server/server.js
@@ -3,10 +3,12 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
+var port = 5000;
+
+
 app.use(express.static('public'));
 
 // Server running
-var port = process.env.PORT || 5000;
 app.listen(port, function () {
     console.log('listening on port: ', port);
 });
